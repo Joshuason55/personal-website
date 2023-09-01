69fad5bc07e01ab749eed9e3b72c4051f158e6e8
Joshua Son <joshuason@umail.ucsb.edu>
2023-08-31 19:46:13 -0700

    Added Helmet

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import './App.css';
 import Home from './components/pages/Home';
-
+import {Helmet} from "react-helmet";
 
 function App() {
   return (
     <div>
-        <Navbar />
-        <Home />
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Joshua's Personal Website</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+        <meta name="description" content="Joshua Son Personal Website" />
+      </Helmet>
+      <Navbar />
+      <Home />
   </div>
   );
 }
