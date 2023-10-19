c9098d37afd7f12ad122acf50a38952d8e13632b
Joshua Son <joshuason@umail.ucsb.edu>
2023-10-19 14:21:15 -0700

    added two projects

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -4,6 +4,8 @@ import './Projects.css'
 import CrystalPicture from './cardphotos/stretchcrystal.png'
 import PersonalWeb from './cardphotos/pwebphoto.png'
 import KeepItGreen from './cardphotos/Keep_It_Green_Horizontal_r.png'
+import Ziptivity from './cardphotos/ZiptivityLogo.png'
+import SpotAppLogo from './cardphotos/SpottoApp.png'
 import { Grid } from '@mui/material'
 import { Fade } from 'react-reveal'
 
@@ -47,6 +49,26 @@ function Projects() {
             title="KeepItGreen"
             description="Created a mobile app leveraging machine learning to identify waste classifications using smartphone camera images."
             language="Python, Java, Firebase"
+          />
+        </Grid>
+        <Grid item xs={12} sm={12} md={6} lg={5} xl={3.5}>
+        <Card
+            photonumber={'img4'}
+            imageUrl={Ziptivity}
+            title="Ziptivity"
+            description="Engineered a web app where users can input their location via zip code and find activities in their location given specific weather conditions."
+            language='React, JavaScript, CSS, OpenAI API, OWM API'
+            url='https://github.com/Joshuason55/ziptivity'
+            githublogo='fab fa-github'
+          />
+        </Grid>
+        <Grid item xs={12} sm={12} md={6} lg={5} xl={3.5}>
+          <Card
+            photonumber={'img5'}
+            imageUrl={SpotAppLogo}
+            title="SpotToApp Converter"
+            description="Created a web app where users can input playlists and songs that they want to transfer from spotify to apple music"
+            language= "React, JavaScript, SpotifyAPI, Apple Music API"
           />
         </Grid>
       </Grid>
